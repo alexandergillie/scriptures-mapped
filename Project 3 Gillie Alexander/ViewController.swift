5bfb31588cfc8654239fa0e2c2523ecd8efa7dac
Alex Gillie <alexander.gillie@insidesales.com>
2018-11-21 15:00:19 -0700

    Initial Commit

---
+++ Project 3 Gillie Alexander/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Project 3 Gillie Alexander
+//
+//  Created by Alexander Gillie on 11/21/18.
+//  Copyright © 2018 Alexander Gillie. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
---
+++ Project 3 Gillie AlexanderTests/Project_3_Gillie_AlexanderTests.swift
@@ -0,0 +1,34 @@
+//
+//  Project_3_Gillie_AlexanderTests.swift
+//  Project 3 Gillie AlexanderTests
+//
+//  Created by Alexander Gillie on 11/21/18.
+//  Copyright © 2018 Alexander Gillie. All rights reserved.
+//
+
+import XCTest
+@testable import Project_3_Gillie_Alexander
+
+class Project_3_Gillie_AlexanderTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
